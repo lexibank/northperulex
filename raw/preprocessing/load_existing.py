7354fcb27ab3b8af7e100193f2728e8540df30a5
Frederic Blum <frederic_blum@eva.mpg.de>
2023-12-06 13:10:04 +0100

    update

--- raw/preprocessing/load_existing.py
+++ raw/preprocessing/load_existing.py
@@ -6,12 +6,13 @@ from lingpy import Wordlist
 
 langs = defaultdict()
 concepts = defaultdict()
-output = []
+output = [["Doculect", "Concept", "Form"]]
+
 visited = []
 with open('../../etc/languages.tsv', mode='r', encoding="utf8") as file:
     data = csv.reader(file, delimiter="\t")
     for lines in data:
-        langs[lines[2]] = lines[0]
+        langs[lines[0]] = lines[4]
 
 BASE = "cldf-data/concepticon-data/concepticondata/conceptlists/"
 SWAD_200 = BASE + "Swadesh-1952-200.tsv"
@@ -39,13 +40,12 @@ wl = Wordlist.from_cldf(
 )
 
 for idx in wl:
-    if wl[idx, "doculect"] in langs:
-        if wl[idx, "concepticon_id"] in concepts:
-            output.append([
-                wl[idx, "doculect"],
-                wl[idx, "concept"],
-                wl[idx, "segments"]
-            ])
+    if wl[idx, "concepticon_id"] in concepts:
+        output.append([
+            wl[idx, "doculect"],
+            wl[idx, "concept"],
+            "".join(wl[idx, "segments"])
+        ])
 
 
 wl = Wordlist.from_cldf(
@@ -66,12 +66,12 @@ wl = Wordlist.from_cldf(
 )
 
 for idx in wl:
-    if wl[idx, "doculect"] in langs and wl[idx, "doculect"] not in visited:
+    if langs[wl[idx, "doculect"]] == 1 and wl[idx, "doculect"] not in visited:
         if wl[idx, "concepticon_id"] in concepts:
             output.append([
                 wl[idx, "doculect"],
                 wl[idx, "concept"],
-                wl[idx, "segments"]
+                "".join(wl[idx, "segments"])
             ])
 
 wl = Wordlist.from_cldf(
@@ -94,43 +94,14 @@ wl = Wordlist.from_cldf(
 )
 
 for idx in wl:
-    if wl[idx, "doculect"] in langs and wl[idx, "doculect"] not in visited:
+    if langs[wl[idx, "doculect"]] == 1 and wl[idx, "doculect"] not in visited:
         if wl[idx, "concepticon_id"] in concepts:
             output.append([
                 wl[idx, "doculect"],
                 wl[idx, "concept"],
-                wl[idx, "segments"]
+                "".join(wl[idx, "segments"])
             ])
 
 with open("imported/loaded_data.csv", 'w', encoding="utf8", newline='') as f:
     writer = csv.writer(f)
     writer.writerows(output)
-
-syns = defaultdict()
-lang_count = defaultdict()
-covered = []
-for item in wl:
-    checkup = [
-        wl[item, "doculect"],
-        wl[item, "concept"]
-        ]
-    if checkup[0] not in syns:
-        syns[checkup[0]] = 0
-        lang_count[checkup[0]] = 0
-    if checkup in covered:
-        syns[checkup[0]] = syns[checkup[0]] + 1
-    else:
-        lang_count[checkup[0]] = lang_count[checkup[0]] + 1
-        covered.append(checkup)
-
-covs = 0
-syno = 0
-for x in syns:
-    coverage = round((lang_count[x] / len(concepts)), 2)
-    covs += coverage
-    synonyms = round(((syns[x]+lang_count[x]) / lang_count[x]), 2)
-    syno += synonyms
-    print("---")
-    print("Language:", x)
-    print("Total coverage", coverage)
-    print("Synonyms:", synonyms)
