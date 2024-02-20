1ff5ba78acbfcacbedb55056f504ca9a5274ec42
MuffinLinwist <carlosbarrientosugarte97@gmail.com>
2024-02-20 14:00:07 +0100

    Iterating over data

--- raw/preprocessing/parse_ura.py
+++ raw/preprocessing/parse_ura.py
@@ -5,6 +5,12 @@ from csvw.dsv import UnicodeDictReader
 data = []
 concepts = defaultdict()
 
+with open('Urarina.csv', mode='r', encoding="utf-8") as file:
+    data = csv.reader(file, delimiter="\t")
+    headers = next(data)
+    for rows in data:
+        print(data)
+
 # Load concept list
 BASE = "/Users/cb17/Library/Application Support/cldf/concepticon/concepticondata/conceptlists/"
 SWAD_200 = BASE + "Swadesh-1952-200.tsv"
@@ -12,16 +18,18 @@ SWAD_200 = BASE + "Swadesh-1952-200.tsv"
 with UnicodeDictReader(SWAD_200, delimiter='\t') as reader:
     for line in reader:
         concepts[line["CONCEPTICON_ID"]] = line['CONCEPTICON_GLOSS']
-        #print(line)
 		
 # Iterate through data and filter entries
-filtered_data = []
 
-with open('Urarina.csv', mode='r', encoding="utf-8") as file:
-    data = csv.reader(file, delimiter="\t")
-    for row in data:
+for row in rows:
+    filtered_data = []
+    
+    for item in concepts:
         if row[7] in concepts:
-            filtered_data.append(row)
+            filtered_data.append([
+                rows[row],
+                concepts[item]
+            ])
             
 for entry in filtered_data:
     print(entry)
