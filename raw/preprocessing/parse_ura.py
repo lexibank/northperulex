8507009f5f707f07883c292b7ab0cab1d3657146
MuffinLinwist <carlosbarrientosugarte97@gmail.com>
2024-02-23 13:51:06 +0100

    First parsing of Urarina data

--- raw/preprocessing/parse_ura.py
+++ raw/preprocessing/parse_ura.py
@@ -5,14 +5,8 @@ from csvw.dsv import UnicodeDictReader
 data = []
 concepts = defaultdict()
 
-with open('Urarina.csv', mode='r', encoding="utf-8") as file:
-    data = csv.reader(file, delimiter="\t")
-    headers = next(data)
-    for rows in data:
-        print(data)
-
 # Load concept list
-BASE = "/Users/cb17/Library/Application Support/cldf/concepticon/concepticondata/conceptlists/"
+BASE = "cldf-data/concepticon-data/concepticondata/conceptlists/"
 SWAD_200 = BASE + "Swadesh-1952-200.tsv"
 
 with UnicodeDictReader(SWAD_200, delimiter='\t') as reader:
@@ -21,16 +15,19 @@ with UnicodeDictReader(SWAD_200, delimiter='\t') as reader:
 		
 # Iterate through data and filter entries
 
-for row in rows:
-    filtered_data = []
-    for item in concepts:
-        # Here is where it breaks. I believe the code is not being able to access the 7th column for each of the roads and
-        # therefore not being able to match it to the concepts dict
+filtered_data = [[
+        "Doculect", "Concept", "Form"
+    ]]
+
+with open('Urarina.csv', mode='r', encoding="utf-8") as file:
+    data = csv.reader(file, delimiter=",")
+    headers = next(data)
+    for row in data:
         if row[7] in concepts:
             filtered_data.append([
-                rows[row],
-                concepts[item]
+                row[1],
+                row[2]
             ])
-            
-for entry in filtered_data:
-    print(entry)
+            with open('Urarina.tsv', 'w', encoding="utf8", newline='') as f:
+                writer = csv.writer(f, delimiter=",")
+                writer.writerows(filtered_data)
