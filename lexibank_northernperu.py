8614d5f78cca1b61e816e45446d8eac1b4fec040
MuffinLinwist <carlosbarrientosugarte97@gmail.com>
2024-06-10 14:05:59 +0200

    Missing languages

--- lexibank_northernperu.py
+++ lexibank_northernperu.py
@@ -144,10 +144,10 @@ class Dataset(BaseDataset):
             if value and '_' not in value:
                 if language not in languages:
                     errors.add(("language", language))
-                    #print(f"Missing language: {language} - Row: {idx}")
+                    print(f"Missing language: {language} - Row: {idx}")
                 elif concept not in concepts:
                     errors.add(("concept", concept))
-                    #print(f"Missing concept: {concept} for language: {language} - Row: {idx}")
+                    print(f"Missing concept: {concept} for language: {language} - Row: {idx}")
                 else:
                     # lexeme = args.writer.add_form_with_segments(
                     args.writer.add_forms_from_value(
