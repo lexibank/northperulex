22a913acfda4a856f52856e515303e3e49a76963
MuffinLinwist <carlosbarrientosugarte97@gmail.com>
2025-03-31 13:58:02 +0200

    correcting total of forms for test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ def test_valid(cldf_dataset, cldf_logger):
 
 
 def test_forms(cldf_dataset):
-    assert len(list(cldf_dataset["FormTable"])) == 4750
+    assert len(list(cldf_dataset["FormTable"])) == 4863
 
 
 def test_parameters(cldf_dataset):
