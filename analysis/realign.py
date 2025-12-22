9dd3564907fcedd2d60380a967bb9807425ef54d
CMBUgarte <carlos_barrientos_ugarte@eva.mpg.de>
2025-12-22 17:37:51 +0100

    align and new output data file

--- analysis/realign.py
+++ analysis/realign.py
@@ -4,18 +4,20 @@ partial cognate detection and multiple sequence alignment
 """
 from lingpy import Alignments
 from lingpy.compare.partial import Partial
+from lexibase.lexibase import LexiBase
 
 def run(wordlist):
-	data = {0: wordlist.columns}
+	D = {0: wordlist.columns}
 	for idx in wordlist:
-		data[idx] = [wordlist[idx, h] for h in data[0]]
+		D[idx] = [wordlist[idx, h] for h in D[0]]
 		
-	lex = Partial(data, segments='tokens', check=False)
+	lex = Partial(D, segments='tokens', check=False)
+
 	lex = Alignments(lex, ref='cogids')
 	lex.align(ref='cogids')
 	
-	aligned_data = {0: wordlist.columns + ["alignment"]}
+	D = {0: wordlist.columns+["alignment"]}
 	for idx in lex:
-		aligned_data[idx] = [lex[idx, h] for h in aligned_data[0]]
+		D[idx] = [lex[idx, h] for h in D[0]]
 		
-	return aligned_data
+	return lex
